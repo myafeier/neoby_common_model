76d67d3c17dd7c23f81d42959a0c7633c2ecbe82
Joffre <hiall@aliyun.com>
2019-01-22 15:49:32 +0800

    fix bug

--- init.go
+++ init.go
@@ -6,64 +6,60 @@ import (
 	"time"
 )
 
-
 var LocalDB *xorm.Engine
 var RemoteDB *xorm.Engine
 
-func InitRemoteDB(ip,port,user,password,dbname string,showSQL bool)(err error) {
-		RemoteDB, err = xorm.NewEngine("mysql", user+":"+password+"@tcp("+ip+":"+port+")/"+dbname+"?charset=utf8mb4")
-		if err != nil {
-			return
-		}
-		RemoteDB.SetMaxIdleConns(10)
-		RemoteDB.SetMaxOpenConns(100)
-		RemoteDB.SetConnMaxLifetime(100 * time.Second)
-		RemoteDB.ShowSQL(showSQL)
+func InitRemoteDB(ip, port, user, password, dbname string, showSQL bool) (err error) {
+	RemoteDB, err = xorm.NewEngine("mysql", user+":"+password+"@tcp("+ip+":"+port+")/"+dbname+"?charset=utf8mb4")
+	if err != nil {
 		return
-}
-
-
-
-func InitLocalDB(ip,port,user,password,dbname string,showSQL bool )(err error) {
-		RemoteDB, err = xorm.NewEngine("mysql", user+":"+password+"@tcp("+ip+":"+port+")/"+dbname+"?charset=utf8mb4")
-		if err != nil {
-			return
-		}
-		RemoteDB.SetMaxIdleConns(10)
-		RemoteDB.SetMaxOpenConns(100)
-		RemoteDB.SetConnMaxLifetime(100*time.Second)
-		RemoteDB.ShowSQL(showSQL)
+	}
+	RemoteDB.SetMaxIdleConns(10)
+	RemoteDB.SetMaxOpenConns(100)
+	RemoteDB.SetConnMaxLifetime(100 * time.Second)
+	RemoteDB.ShowSQL(showSQL)
 	return
 }
 
+func InitLocalDB(ip, port, user, password, dbname string, showSQL bool) (err error) {
+	LocalDB, err = xorm.NewEngine("mysql", user+":"+password+"@tcp("+ip+":"+port+")/"+dbname+"?charset=utf8mb4")
+	if err != nil {
+		return
+	}
+	LocalDB.SetMaxIdleConns(10)
+	LocalDB.SetMaxOpenConns(100)
+	LocalDB.SetConnMaxLifetime(100 * time.Second)
+	LocalDB.ShowSQL(showSQL)
+	return
+}
 
 //初始化本元账户of 收益
-func InitNeobyAccountOfProfitAmount()(err error){
+func InitNeobyAccountOfProfitAmount() (err error) {
 	//初始化公司账户
-	pa:=&ProfitAmount{}
-	pa.Id=1
-	pa.IsCompany=1
-	pa.Type=ProfitAmountAccountTypeOfNeoby
-	has,err:=LocalDB.Get(pa)
-	if err!=nil{
+	pa := &ProfitAmount{}
+	pa.Id = 1
+	pa.IsCompany = 1
+	pa.Type = ProfitAmountAccountTypeOfNeoby
+	has, err := LocalDB.Get(pa)
+	if err != nil {
 		return
 	}
-	if !has{
-		_,err=LocalDB.Insert(pa)  //初始化
-		if err!=nil{
+	if !has {
+		_, err = LocalDB.Insert(pa) //初始化
+		if err != nil {
 			return
 		}
 	}
 	return
 }
 
-func UpdateRemoteDbStructOfTable()(err error){
-	_,err=RemoteDB.Exec("alter table jyb_store add column profit_freeze_day int(11)")
+func UpdateRemoteDbStructOfTable() (err error) {
+	_, err = RemoteDB.Exec("alter table jyb_store add column profit_freeze_day int(11)")
 	return
 }
 
-func CreateLocalDBTable()(err error){
-	var tables = []interface{}{ProfitAmount{}, ProfitAmountLog{},Reward{},Withdraw{},Order{}}
+func CreateLocalDBTable() (err error) {
+	var tables = []interface{}{ProfitAmount{}, ProfitAmountLog{}, Reward{}, Withdraw{}, Order{}}
 	//DB.Sync2(tables)
 
 	var isExist bool
